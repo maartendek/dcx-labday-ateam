786ee58cfb64ffdc0fe290543a378b5fce487315
ASCHAAF <Katwijk2221MVJuly>
2021-12-17 12:58:17 +0100

    Quizpagina

--- pages/quiz.vue
+++ pages/quiz.vue
@@ -4,14 +4,23 @@
       <h1>Quiz</h1>
 	    <div class="question_field">
 		    <div class="question_intro">
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec luctus nisl. Etiam gravida rhoncus urna nec congue. Aliquam blandit feugiat diam, eu lobortis orci feugiat eu. Sed enim nisl.
+          </p>
 		    </div>
 		    <div class="question_img">
+          <img src="../img/i_heart_grogu.jpg" alt="sweet baby yoda">
 		    </div>
 		    <div class="question">
+          <p>There is no try
+          </p>
 		    </div>
 	    </div>	
 	    <div class="answer_field">
 		    <div class="answer_btns">
+          <button class="answer_btn" type="button">Do!</button>
+          <button class="answer_btn" type="button">Or Do Not!</button>
+          <button class="answer_btn" type="button">Click Me!</button>
+          <button class="answer_btn" type="button">No, Click Me!</button>
 		    </div>
 	    </div>
     </div>
