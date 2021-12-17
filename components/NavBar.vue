b2e5641f655afbda73b5b7623d2a2578046ab3bc
Florian van der Wielen <fvanderwielen@gmail.com>
2021-12-17 12:26:44 +0100

    refix the link to the settings page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav>
       <ul>
           <li><NuxtLink to="/">Home</NuxtLink></li>
-          <li><NuxtLink to="/instellingen">Settings</NuxtLink></li>
+          <li><NuxtLink to="/settings">Settings</NuxtLink></li>
           <li><NuxtLink to="/quiz">Quiz</NuxtLink></li>
           <li><NuxtLink to="/score">Score</NuxtLink></li>
       </ul>
