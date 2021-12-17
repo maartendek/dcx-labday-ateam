a11e650e07aa5c9f66fbef48e2ea4e91e893c8cd
fjorden <firenzo.jorden@capgemini.com>
2021-12-17 11:34:33 +0100

    basic navigation

---
+++ components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+      <ul>
+          <li><NuxtLink to="/instellingen">instellingen</NuxtLink></li>
+          <li><NuxtLink to="/quiz">Quiz</NuxtLink></li>
+          <li><NuxtLink to="/score">Score</NuxtLink></li>
+      </ul>
+  </nav>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,44 @@
 <template>
-  <Nuxt />
+  <div id="app">
+      <NavBar />
+      <Nuxt />
+  </div>
 </template>
+
+<script>
+export default {
+
+  head () {
+    return {
+      title: 'Team Uitbehandeld',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'informatie voor uitbehandelde mensen en hun families'
+        }
+      ]
+    }
+  },
+
+  data: () => ({
+
+  }),
+
+  computed: {
+
+  },
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
