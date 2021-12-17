aa9d85a8cc2ea359d25119a2fb8cc8d7db75cf37
fjorden <firenzo.jorden@capgemini.com>
2021-12-17 11:41:36 +0100

    added home link

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
       <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/instellingen">instellingen</NuxtLink></li>
           <li><NuxtLink to="/quiz">Quiz</NuxtLink></li>
           <li><NuxtLink to="/score">Score</NuxtLink></li>
